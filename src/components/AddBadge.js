470f2e923f45cae4a140a18c8414bc726c646bb7
chris <cseiter@gmail.com>
2021-08-21 16:04:20 -0400

    copy AddBadge.js to EditBadge.js to keep matching styles between forms

--- src/components/AddBadge.js
+++ src/components/AddBadge.js
@@ -5,7 +5,7 @@ import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 export const AddBadge = () => {
     return (
         <div className="text-center" style={{maxWidth: "90%", margin: "10px auto", border: "2px solid green"}}>
-            <h1> Add Badge</h1>
+            <h1>Add Badge</h1>
             <Form>
                 <FormGroup>
                     <Label>Name</Label>
--- src/components/EditBadge.js
+++ src/components/EditBadge.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
 export const EditBadge = () => {
     return (
-        <div>
-            <h1>Edit Badge</h1> 
-        </div>
+    <div className="text-center" style={{maxWidth: "90%", margin: "10px auto", border: "2px solid green"}}>
+    <h1>Edit Badge</h1>
+    <Form>
+        <FormGroup>
+            <Label>Name</Label>
+            <Input type="text" placeholder="enter badge name, with underscore replacing a space"></Input>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+    </Form>
+    </div>
     )
 }
