8f7d15bcf89da7ccfa418b23358fc5a4913c86a5
chris <cseiter@gmail.com>
2021-08-21 17:05:33 -0400

    create Navbar items in Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavbarBrand, Container } from 'reactstrap';
 
 export const Heading = () => {
     return (
-        <div>
-            <h1>Heading</h1>
-        </div>
+        <Navbar color="dark" dark>
+            <Container>
+                <NavbarBrand href="/">My Badges</NavbarBrand>
+                <Nav>
+                    <NavItem>
+                        <Link className="btn btn-primary" to="/addbadge">Add Badge</Link>
+                    </NavItem>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Heading } from './Heading';
+import { BadgeList } from './BadgeList';
 
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Heading />
+            <BadgeList />
         </div>
     )
 }
