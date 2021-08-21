0ea793e0cf40864a71069660eee1b0e0d6ad80e8
chris <cseiter@gmail.com>
2021-08-21 14:24:07 -0400

    import components into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Home} from './components/Home';
+import {AddBadge} from './components/AddBadge';
+import {EditBadge} from './components/EditBadge';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Merit Badge Tracker</h1>
+      <Home />
+      <AddBadge />
+      <EditBadge />
     </div>
   );
 }
