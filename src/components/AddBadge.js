ba91007932054b5db0244a9db7ada9797bc2ac0e
chris <cseiter@gmail.com>
2021-08-21 15:51:02 -0400

    create initial form in AddBadge.js

--- src/components/AddBadge.js
+++ src/components/AddBadge.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
 export const AddBadge = () => {
     return (
         <div>
             <h1> Add Badge</h1>
+            <Form>
+                <FormGroup>
+                    <Label>Name</Label>
+                    <Input type="text" placeholder="enter badge name, with underscore replacing a space"></Input>
+                </FormGroup>
+                <Button type="submit">Submit</Button>
+            </Form>
         </div>
     )
 }
