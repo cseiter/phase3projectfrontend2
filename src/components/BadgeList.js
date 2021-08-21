4ec4f688dad7414938d1bd3d454ae4b9392b158e
chris <cseiter@gmail.com>
2021-08-21 17:26:52 -0400

    create one item list in BadgeList.js

--- src/components/BadgeList.js
+++ src/components/BadgeList.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {ListGroup, ListGroupItem, Button} from 'reactstrap';
 
 export const BadgeList = () => {
     return (
-        <div>
-            <h1>BadgeList</h1>
-        </div>
+        <ListGroup>
+            <ListGroupItem className="d-flex">
+                <strong>Badge 1</strong>
+            <div className="ml-auto">
+                <Link className="btn btn-warning" to="/edit/1">Edit</Link>
+                <Button color="danger">X</Button>
+            </div>
+            </ListGroupItem>
+        </ListGroup>
     )
 }
