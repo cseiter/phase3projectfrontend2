57189b6ee0032cf0608308d6b79b350a09438a18
chris <cseiter@gmail.com>
2021-08-21 15:19:23 -0400

    import bootstrap into App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Home} from './components/Home';
 import {AddBadge} from './components/AddBadge';
 import {EditBadge} from './components/EditBadge';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
